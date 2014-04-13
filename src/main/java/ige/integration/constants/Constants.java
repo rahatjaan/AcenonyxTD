cacb670811c24d6d7d7c9b6bcb8b717c8a15f6fc
Rahat Ali <rahat.jaan@gmail.com>
2014-04-13 17:17:23 +0500

    Commit

---
+++ src/main/java/ige/integration/constants/Constants.java
@@ -0,0 +1,30 @@
+package ige.integration.constants;
+
+public class Constants {
+	public static final String GUESTCHECKIN = "GuestCheckInFlow";
+	public static final String GUESTPLACEORDER = "GuestPlaceOrderFlow";
+	public static final String GUESTBILLINFO = "GuestBillInfoFlow";
+	public static final String IGE_GUESTBILLINFO = "IGEGuestBillInfoFlow";
+	public static final String GUESTCHECKOUT = "GuestCheckout";
+	public static final String RESERVLOOKUP = "ReservationLookup";
+	public static final String SENDEMAIL = "SendEmail";
+	public static final String SEND_BILL_INFO_EMAIL = "SendBillInfoEmail";
+	public static final String HOTELFOLIO = "HotelFolio";
+	public static final String PAYMENTCARDPROCESSING = "PaymentCardProcessing";
+	public static final String REPORTPROBLEM = "ReportProblem";
+	public static final String GETGUESTSTAYINFO = "GetGuestStayInfo";
+	public static final String GETLISTROOMS = "GetListRooms";
+	public static final String GUESTSIGNATURE = "GuestSignature";
+	public static final String USERPICTURE = "UserPicture";
+	public static final String UPDATEGUESTSTAYINFO = "UpdateGuestStayInfo";
+<<<<<<< HEAD:src/main/java/com/integration/td/constants/Constants.java
+	public static final String RECEIVETDORDER = "ReceiveTDOrder";
+	public static final String FETCHDYNAMICMENU = "FetchDynamicMenu";
+	public static final String COMMODITYGROUPS = "CommodityGroups";
+	
+	
+	public static final String HOST = "https://www.koronacloud.com/web/api/v1/";
+	public static final String TOKEN = "01ue0m7gdy0ooo0nir2rs33ofrw01bvb7t6g71tzd014gkp0d25ahr3";
+=======
+>>>>>>> 9011a142512171f71dc9078d9bf19ce5020fa952:src/main/java/ige/integration/constants/Constants.java
+}
